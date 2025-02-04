e9c145c174aa2573a12399c85f27020f9e085d65
Karthik DR <karthikdr75@gmail.com>
2025-02-04 17:07:19 +0530

    atm

--- atm.py
+++ atm.py
@@ -31,9 +31,59 @@ class ATMController:
       except ValueError:
         print('Please enter a valid number.')
 
+  
+  def display_menu(self):
+    print('\nWelcome to the ATM!')
+    print('1. Check Balance')
+    print('2. Deposit')
+    print('3. Withdraw')
+    print('4. Exit')    
+
+  def check_balance(self):
+    balance = self.atm.check_balance()
+    print(f'Your current balance is: ${balance}')    
+
+  def deposit(self):
+    while True:
+      try:
+        amount = self.get_number('Enter the amount to deposit: ')
+        self.atm.deposit(amount)
+        print(f'Successfully deposited ${amount}.')
+        break
+      except ValueError as error:
+        print(error)    
+
+  def withdraw(self):
+    while True:
+      try:
+        amount = self.get_number('Enter the amount to withdraw: ')
+        self.atm.withdraw(amount)
+        print(f'Successfully withdrew ${amount}.')
+        break
+      except ValueError as error:
+        print(error)    
+
+  def run(self):
+    while True: 
+      self.display_menu()
+
+      choice = input('Please choose an option: ')
+      if choice == '1':
+        self.check_balance()
+      elif choice == '2':
+        self.deposit()
+      elif choice == '3':
+        self.withdraw()
+      elif choice == '4':
+        print('Thank you for using the ATM.')
+        break
+      else:
+        print('Invalid choice. Please try again.')
+
 
 def main():
   atm = ATMController()
+  atm.run()
 
 
 if __name__ == '__main__':
