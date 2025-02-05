fc1e919139166b1c6a773843c72fcf4ab1047b4b
Karthik DR <karthikdr75@gmail.com>
2025-02-05 09:33:21 +0530

    password generator

---
+++ password_generator.py
@@ -0,0 +1,9 @@
+def main():
+  length = int(input('Enter password length: '))
+  include_uppercase = input('Include uppercase letters? (y/n): ').lower() == 'y'
+  include_numbers = input('Include numbers? (y/n): ').lower() == 'y'
+  include_special = input('Include special characters? (y/n): ').lower() == 'y'
+
+
+if __name__ == '__main__':
+  main()
